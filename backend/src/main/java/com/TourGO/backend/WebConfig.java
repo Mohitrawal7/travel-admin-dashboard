86708218b471af0a638beca2426a5cb176a883c5
Mohitrawal7 <rawalmohit12@gmail.com>
2026-02-28 09:51:14 +0545

    deploy:step1 --main

--- backend/src/main/java/com/TourGO/backend/WebConfig.java
+++ backend/src/main/java/com/TourGO/backend/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-//                .allowedOrigins("https://travel-admin-dashboard-delta.vercel.app")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://travel-admin-dashboard-delta.vercel.app")
+//                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
